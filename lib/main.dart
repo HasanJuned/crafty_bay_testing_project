964728fa3df91100891fec6328995b5db8d680e7
Hassan <hasanahmad9812021@gmail.com>
2023-11-15 21:24:09 +0600

    E-Commerce

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main(){
+  runApp(CraftyBay());
+}
+
+class CraftyBay extends StatelessWidget {
+  const CraftyBay({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
+
